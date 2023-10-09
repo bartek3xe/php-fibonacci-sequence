423bedd3443e731a9e08f237ef500c0e397e56ba
bartek3xe <bartek3xe@gmail.com>
2023-10-09 17:43:59 +0200

    Create basic controller and templates

---
+++ src/Controller/FibonacciController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\FibonacciType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FibonacciController extends AbstractController
+{
+    #[Route('/fibonacci', name: 'fibonacci')]
+    public function index(Request $request): Response
+    {
+        $form     = $this->createForm(FibonacciType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+        }
+
+        return $this->render('fibonacci/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
