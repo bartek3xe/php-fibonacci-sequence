2ccb38da61ac0da7ab4104e4d5e10a887bd7d055
bartek3xe <bartek3xe@gmail.com>
2023-10-09 15:05:13 +0200

    Create enum with fibonacci calc types

---
+++ src/DBAL/Enum/FibonacciEnumType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DBAL\Enum;
+
+enum FibonacciEnumType
+{
+    const TYPE_REQUIRES_INPUT         = 1;
+    const TYPE_DOES_NOT_REQUIRE_INPUT = 0;
+
+    const CALC_FIBONACCI_BY_INPUT = 'Own input value';
+    const CALC_FIBONACCI_BY_DAY   = 'Day';
+    const CALC_FIBONACCI_BY_MONTH = 'Month';
+
+    const FIBONACCI_CALC_TYPES = [
+        self::CALC_FIBONACCI_BY_INPUT => self::TYPE_REQUIRES_INPUT,
+        self::CALC_FIBONACCI_BY_MONTH => self::TYPE_DOES_NOT_REQUIRE_INPUT,
+        self::CALC_FIBONACCI_BY_DAY   => self::TYPE_DOES_NOT_REQUIRE_INPUT,
+    ];
+}
