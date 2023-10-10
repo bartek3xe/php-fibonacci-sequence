a3bc407599d8e500d7718459365e746456a09632
bartek3xe <bartek3xe@gmail.com>
2023-10-10 23:37:14 +0200

    Restore file deleted by mistake

---
+++ src/Controller/FibonacciController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\DBAL\Enum\FibonacciEnumType;
+use App\Fibonacci\Service\FibonacciSequenceCalculator;
+use App\Form\FibonacciType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FibonacciController extends AbstractController
+{
+
+    public function __construct(private readonly FibonacciSequenceCalculator $fibonacciSequenceCalculator)
+    {
+    }
+
+    #[Route('/fibonacci', name: 'fibonacci')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(FibonacciType::class);
+
+        $form->handleRequest($request);
+
+        $result = null;
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data   = $form->getData();
+            $result = $this->fibonacciSequenceCalculator->calculateBasedOnType($data['calcType'], $data['number']);
+        }
+
+        return $this->render('fibonacci/index.html.twig', [
+            'form'      => $form,
+            'calcTypes' => FibonacciEnumType::FIBONACCI_CALC_TYPES,
+            'result'    => $result,
+        ]);
+    }
+}
