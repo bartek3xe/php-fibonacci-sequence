011c8aba452cbc922fe9d29581ba3b3d4229c558
bartek3xe <bartek3xe@gmail.com>
2023-10-10 23:34:44 +0200

    Write tests for controller and service

---
+++ tests/Service/FibonacciSequenceCalculatorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Fibonacci\Service\FibonacciSequenceCalculator;
+
+class FibonacciSequenceCalculatorTest extends TestCase
+{
+    public function testCalculateBasedOnTypeWithOwnValue()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateBasedOnType(null, 5);
+        $this->assertEquals("Suma ciągu to 7", $result);
+    }
+
+    public function testCalculateBasedOnTypeWithDayValue()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateBasedOnType('day', null);
+
+        $this->assertIsString($result);
+    }
+
+    public function testCalculateBasedOnTypeWithMonthValue()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateBasedOnType('month', null);
+
+        $this->assertIsString($result);
+    }
+
+    public function testCalculateBasedOnTypeWithInvalidNumber()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateBasedOnType('day', 1000);
+        $this->assertEquals("Liczba nie może być większa od 44", $result);
+    }
+
+    public function testCalculateForOwnValue()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateForOwnValue(5);
+        $this->assertEquals(7, $result);
+    }
+
+    public function testCalculateForDay()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateForDay();
+        $this->assertIsInt($result);
+    }
+
+    public function testCalculateForMonth()
+    {
+        $calculator = new FibonacciSequenceCalculator();
+        $result     = $calculator->calculateForMonth();
+        $this->assertIsInt($result);
+    }
+}
