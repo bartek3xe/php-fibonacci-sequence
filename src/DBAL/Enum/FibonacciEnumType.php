1d4e26db524d33f5bc86d64889578666f15a91e7
bartek3xe <bartek3xe@gmail.com>
2023-10-10 18:26:42 +0200

    Refactor enum, simplify code
    I needed to change values because i cannot identity specific field
    based on value 0 or 1. I need to have information about specific
    type of calculation like month, day keywords or null value.

--- src/DBAL/Enum/FibonacciEnumType.php
+++ src/DBAL/Enum/FibonacciEnumType.php
@@ -6,16 +6,17 @@ enum FibonacciEnumType
 {
     const MAX_INPUT_NUMBER = 44;
 
-    const TYPE_REQUIRES_INPUT         = 1;
-    const TYPE_DOES_NOT_REQUIRE_INPUT = 0;
+    const DAY_KEY   = "today's day";
+    const MONTH_KEY = "today's month";
+    const OWN_KEY   = "Own input value";
 
-    const CALC_FIBONACCI_BY_INPUT = 'Own input value';
-    const CALC_FIBONACCI_BY_DAY   = 'Day';
-    const CALC_FIBONACCI_BY_MONTH = 'Month';
+    const DAY_VALUE   = 'day';
+    const MONTH_VALUE = 'month';
+    const OWN_VALUE   = null;
 
     const FIBONACCI_CALC_TYPES = [
-        self::CALC_FIBONACCI_BY_INPUT => self::TYPE_REQUIRES_INPUT,
-        self::CALC_FIBONACCI_BY_MONTH => self::TYPE_DOES_NOT_REQUIRE_INPUT,
-        self::CALC_FIBONACCI_BY_DAY   => self::TYPE_DOES_NOT_REQUIRE_INPUT,
+        self::DAY_KEY   => self::DAY_VALUE,
+        self::MONTH_KEY => self::MONTH_VALUE,
+        self::OWN_KEY   => self::OWN_VALUE,
     ];
 }
